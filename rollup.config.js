02e611c898ae25a6f7bf3da8e85c40bad76c7c9a
abloom25 <evereen2023@gmail.com>
2025-07-08 12:55:07 +0800

    build: 添加exports字段并更新rollup配置
    在package.json中添加exports字段以支持ES模块和CommonJS模块的导入
    同时更新rollup配置文件的输出文件名以匹配exports字段

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import typescript from '@rollup/plugin-typescript';
 export default {
   input: 'src/glowdown.ts',
   output: {
-    file: 'dist/glowdown.browser.js',
+    file: 'dist/glowdown.js',
     format: 'umd',
     name: 'Glowdown',
     globals: {
